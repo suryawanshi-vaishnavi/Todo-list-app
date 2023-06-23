3f346ad6b28bb66010fd5a9cd9c07d9db70294a4
Vaishnavi Suryawanshi <vaishnavishruti121@gmail.com>
2023-06-23 16:41:54 +0530

    Todo App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,135 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const initialTodos = [];
+
+  const [todos, setTodos] = React.useState(initialTodos);
+  const [todo, setTodo] = React.useState("");
+  const [todoEditing, setTodoEditing] = React.useState(null);
+  const [editingText, setEditingText] = React.useState("");
+
+  useEffect(() => {
+    const json = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(json);
+    if (loadedTodos) {
+      setTodos(loadedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    const json = JSON.stringify(todos);
+    localStorage.setItem("todos", json);
+  }, [todos]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  }
+
+  function deleteTodo(id) {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  }
+
+  function toggleComplete(id) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    const sortedTodos = updatedTodos.sort((a, b) => {
+      if (a.completed && b.completed) {
+        return b.id - a.id; 
+      }
+      if (a.completed) {
+        return 1; 
+      }
+      if (b.completed) {
+        return -1; 
+      }
+      return b.id - a.id; 
+    });
+
+    setTodos(sortedTodos);
+  }
+
+  function submitEdits(id) {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.text = editingText;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    setTodoEditing(null);
+  }
+
+  function handleReset() {
+    setTodos([]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div id="todo-list">
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => (
+        <div
+          key={todo.id}
+          className={`todo ${todo.completed ? "completed" : ""}`}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="todo-text">
+            <input
+              type="checkbox"
+              id="completed"
+              checked={todo.completed}
+              onChange={() => toggleComplete(todo.id)}
+            />
+            {todo.id === todoEditing ? (
+              <input
+                type="text"
+                onChange={(e) => setEditingText(e.target.value)}
+              />
+            ) : (
+              <div>{todo.text}</div>
+            )}
+          </div>
+          <div className="todo-actions">
+            {todo.id === todoEditing ? (
+              <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
+            ) : (
+              <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+            )}
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </div>
+          <div id="todo-list">
+      <h1>Todo List</h1>
+      <div className="reset-button-container">
+        <button className="reset-button" onClick={handleReset}>
+          Reset
+        </button>
+      </div>
+    
+    </div>
+        </div>
+      ))}
     </div>
   );
 }
